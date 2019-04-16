2615134ac509ccdbe973fd36a4db145f17211453
nosoosso <nosoosso.null@gmail.com>
2019-04-16 20:48:50 +0900

    Add solution

--- contest/src/main/scala/leetcode/p0001_two_sum/Main.scala
+++ contest/src/main/scala/leetcode/p0001_two_sum/Main.scala
@@ -1,12 +1,16 @@
 package leetcode.p0001_two_sum
 
 object Main extends App {
-  val r = Solution.twoSum(Array(2, 7, 11, 15), 9)
-  println(r.mkString(","))
+  //  val r = Solution.slowTwoSum(Array(2, 7, 11, 15), 9)
+  //  println(r.mkString(","))
+
+    val r = Solution.bruteForce(Array(2, 7, 11, 15), 9)
+    println(r.mkString)
+
 }
 
 object Solution {
-  def twoSum(nums: Array[Int], target: Int): Array[Int] = {
+  def slowTwoSum(nums: Array[Int], target: Int): Array[Int] = {
     val indexed = nums.toStream.zipWithIndex
 
     val (ra, rb) = (for {
@@ -19,5 +23,17 @@ object Solution {
       .get
 
     Array(ra._2, rb._2)
+  }
+
+  def bruteForce(nums: Array[Int], target: Int): Array[Int] = {
+    var r = Array[Int]()
+    for (i <- nums.indices) {
+      for (j <- (i + 1) until nums.length) {
+        if (nums(j) == target - nums(i)) {
+          r = Array(i, j)
+        }
+      }
+    }
+    r
   }
 }
