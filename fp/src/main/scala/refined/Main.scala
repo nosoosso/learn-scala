300fcc7852bd6441a545ecffbb9525a61b059568
nosoosso <nosoosso.null@gmail.com>
2019-06-26 11:42:03 +0900

    Add refined example

--- fp/src/main/scala/refined/Main.scala
+++ fp/src/main/scala/refined/Main.scala
@@ -3,22 +3,36 @@ package refined
 import eu.timepit.refined._
 import eu.timepit.refined.api.Refined
 import eu.timepit.refined.auto._
+import eu.timepit.refined.boolean.AllOf
+import eu.timepit.refined.collection.MaxSize
 import eu.timepit.refined.numeric._
 import eu.timepit.refined.string._
+import shapeless.{HNil, ::}
 
 object Main extends App {
-  type UrlString = String Refined Url
+  def shotUrl: Unit = {
+    type ShortUrl = String Refined AllOf[Url :: MaxSize[W.`20`.T] :: HNil]
 
-  def isHttps(url: UrlString) = {
-    url.startsWith("https://")
+    val valid: ShortUrl = "http://example.com"
+    //    val notUrl: ShortUrl = "hogehogehoge"
+    //    val tooLongUrl: ShortUrl = "http://goooooooooooooooogle.com"
+
   }
 
-  println(isHttps("http://example.com"))
-  println(isHttps("https://example.com"))
+  def url: Unit = {
+    type UrlString = String Refined Url
 
-  // compilation error
-  //  val a = "hoge"
-  //  println(isHttps(a))
+    def isHttps(url: UrlString) = {
+      url.startsWith("https://")
+    }
 
+    println(isHttps("http://example.com"))
+    println(isHttps("https://example.com"))
+
+    // compilation error
+    //  val a = "hoge"
+    //  println(isHttps(a))
+
+  }
 
 }
